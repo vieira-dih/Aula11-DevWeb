5d43de7b37032e01206011cccce709919595832b
vieira-dih <diogovieira03@icloud.com>
2025-05-19 21:15:14 -0300

    changes

--- app/Models/TodoModel.js
+++ app/Models/TodoModel.js
@@ -3,6 +3,26 @@ import { DataTypes } from 'sequelize';
 
 export default (function () {
 
+    return sequelize.define(
+        "TodoModel",
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false
+                },
+                is_checked: {
+                    type: DataTypes.BOOLEAN,
+                    allowNull: false,
+                    defaultValue: false
+                },
+            }       
+    )
+
     // sequelize.define("Nome", campos: {campo: {...}}, opcoes: {...})
 
     // Campos:
